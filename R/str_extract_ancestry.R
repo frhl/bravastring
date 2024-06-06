021e7628ebcb29b66f14830981d04af040c00c10
Frederik Lassen <flassen@flassen-271.local>
2024-06-06 17:31:08 +0100

    fixed typo

--- R/str_extract_ancestry.R
+++ R/str_extract_ancestry.R
@@ -13,7 +13,7 @@
 str_extract_ancestry <- function(x){
   ancestries <- get_ancestries()
   regex_pattern <- regex_vector(c(tolower(ancestries), toupper(ancestries)))
-  regex_pattern <- paste("(_|\\.)?(",regex_pattern,")(_|\\.)")
+  regex_pattern <- paste0("(_|\\.)?(",regex_pattern,")(_|\\.)")
   out <- stringr::str_extract(x, pattern = regex_pattern)
   out <- gsub("(\\.)|(_)", "", out)
   return(out)
