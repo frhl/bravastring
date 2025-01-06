17e4a1f5df2268bb0bfbf019ddc99e8c1571824e
Frederik Lassen <flassen@flassen-331.local>
2025-01-06 11:28:20 +0000

    added extract cases/controls from file name

---
+++ R/str_extract_cases.R
@@ -0,0 +1,20 @@
+#' Extract Number of Cases from a String
+#'
+#' This function extracts the number of cases from a given string using a regular expression pattern.
+#' It looks for a number that appears before a dot and is surrounded by either dots or underscores
+#' in file naming patterns like "UKB.EUR.ALL.4490.218383.CervCanc".
+#'
+#' @param x A character string from which to extract the number of cases.
+#' @return A character string with the extracted number of cases, or NA if no match is found.
+#' @examples
+#' \dontrun{
+#' extracted_cases <- str_extract_cases("UKB.EUR.ALL.4490.218383.CervCanc")
+#' print(extracted_cases)
+#' # [1] "4490"
+#' }
+#' @import stringr
+#' @export
+str_extract_cases <- function(x) {
+  regex_pattern <- "(?<=[_\\.])[0-9]+(?=\\.\\d+[_\\.])"
+  return(stringr::str_extract(x, pattern=regex_pattern))
+}
---
+++ R/str_extract_controls.R
@@ -0,0 +1,20 @@
+#' Extract Number of Controls from a String
+#'
+#' This function extracts the number of controls from a given string using a regular expression pattern.
+#' It looks for a number that appears after a dot and is surrounded by either dots or underscores
+#' in file naming patterns like "UKB.EUR.ALL.4490.218383.CervCanc".
+#'
+#' @param x A character string from which to extract the number of controls.
+#' @return A character string with the extracted number of controls, or NA if no match is found.
+#' @examples
+#' \dontrun{
+#' extracted_controls <- str_extract_controls("UKB.EUR.ALL.4490.218383.CervCanc")
+#' print(extracted_controls)
+#' # [1] "218383"
+#' }
+#' @import stringr
+#' @export
+str_extract_controls <- function(x) {
+  regex_pattern <- "(?<=\\.[0-9]+)[0-9]+(?=[_\\.])"
+  return(stringr::str_extract(x, pattern=regex_pattern))
+}
